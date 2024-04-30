45b15985981f4668b36e3ec21748fcb441a50b34
Remi <prialgauskis.remigijus@gmail.com>
2024-04-30 13:27:16 +0300

    Fixed tailwind css config

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,7 @@ app.use(express.json());
 
 // Database connection
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URI, {})
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
