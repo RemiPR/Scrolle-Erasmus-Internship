99980f7f04fccc14f300d908924d8b7f16393bd0
Litvis <deividaslitvinenko4@gmail.com>
2024-05-08 15:03:52 +0300

    Design updates on the landing page

--- frontend/components/index/LeftTextAd.vue
+++ frontend/components/index/LeftTextAd.vue
@@ -1,20 +1,18 @@
 <template>
   <section>
-    <div class="mx-48 flex justify-between items-center">
-      <div class="w-1/3 mt-80">
+    <div
+      class="mx-4 sm:mx-8 md:mx-16 lg:mx-24 xl:mx-32 2xl:mx-48 flex flex-col lg:flex-row justify-between items-center text-black dark:text-white"
+    >
+      <div class="w-50% lg:w-1/2 lg:pr-4 lg:order-first">
         <h2 class="text-4xl mb-2">Free courses</h2>
-        <p class="text-xl text-white">
+        <p class="text-xl">
           Learn for free from Scroll's free courses which are made by the best
           tutors in Latvia.
         </p>
       </div>
-      <div class="w-1/3 mt-80">
+      <div class="w-50% lg:w-1/2 mt-4 lg:mt-0 lg:order-last">
         <!-- Your photo goes here -->
-        <img
-          src="@/public/potatoes.png"
-          alt="Your Photo"
-          class="w-full h-auto"
-        />
+        <img src="@/public/potatoes.png" alt="Your Photo" />
       </div>
     </div>
   </section>
