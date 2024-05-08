84ada59fab8072ddffddd1f25d5b3e7bbe4b1fa8
Litvis <deividaslitvinenko4@gmail.com>
2024-05-08 17:38:44 +0300

    Tweaked the design on the landing page

---
+++ frontend/components/index/TextLeftAd.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <div
+      class="mx-4 sm:mx-8 md:mx-16 lg:mx-24 xl:mx-32 2xl:mx-48 flex flex-col lg:flex-row justify-between items-center text-black dark:text-white"
+    >
+      <div class="w-25% lg:w-1/2 mt-4 lg:mt-0">
+        <h2 class="text-4xl mb-2">{{ Subtitle1 }}</h2>
+        <p class="text-xl mb-8">
+          {{ Paragraph1 }}
+        </p>
+      </div>
+      <div class="w-25% lg:w-1/3 lg:pr-4">
+        <!-- Your photo goes here -->
+        <img :src="Photo1" alt="Your Photo" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+defineProps({ Subtitle1: String, Paragraph1: String, Photo1: String });
+</script>
--- frontend/layouts/nav.vue
+++ frontend/layouts/nav.vue
@@ -1,42 +1,37 @@
-<!-- layouts/nav.vue -->
 <template>
-  <div>
-    <!-- Header (Nav Menu) -->
-    <header
-      class="flex justify-between items-center p-4 bg-white dark:bg-gray-800 shadow sticky top-0 z-10"
-    >
-      <nav class="flex items-center">
-        <NuxtLink to="#" class="flex items-center">
-          <Icon
-            name="fluent:hat-graduation-24-regular"
-            class="mr-2 h-10 w-10 text-black dark:text-white"
-            alt="Graduation hat icon"
-          />
-          <span class="text-3xl text-black dark:text-white font-semibold"
-            >Scroll</span
-          >
-        </NuxtLink>
-        <NuxtLink
-          to="/"
-          class="text-gray-800 ml-5 dark:text-white hover:underline"
-          >Home</NuxtLink
+  <header
+    class="flex justify-between items-center p-4 bg-white dark:bg-gray-800 shadow"
+    style="position: sticky; top: 0; z-index: 50"
+  >
+    <nav class="flex items-center">
+      <NuxtLink to="#" class="flex items-center">
+        <Icon
+          name="fluent:hat-graduation-24-regular"
+          class="mr-2 h-10 w-10 text-black dark:text-white"
+          alt="Graduation hat icon"
+        />
+        <span class="text-3xl text-black dark:text-white font-semibold"
+          >Scroll</span
         >
-      </nav>
-      <nav class="flex items-center gap-4">
-        <NuxtLink to="/" class="text-gray-800 dark:text-white hover:underline"
-          >Language</NuxtLink
-        >
-        <NuxtLink
-          to="/login"
-          class="text-gray-800 dark:text-white hover:underline"
-          >Login</NuxtLink
-        >
-        <ColorModeToggle />
-      </nav>
-    </header>
-    <!-- Page Content -->
-    <NuxtPage />
-  </div>
+      </NuxtLink>
+      <NuxtLink
+        to="/"
+        class="text-gray-800 ml-5 dark:text-white hover:underline"
+        >Home</NuxtLink
+      >
+    </nav>
+    <nav class="flex items-center gap-4">
+      <NuxtLink to="/" class="text-gray-800 dark:text-white hover:underline"
+        >Language</NuxtLink
+      >
+      <NuxtLink
+        to="/login"
+        class="text-gray-800 dark:text-white hover:underline"
+        >Login</NuxtLink
+      >
+      <ColorModeToggle />
+    </nav>
+  </header>
 </template>
 
 <script setup></script>
