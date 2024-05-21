5876ec1bebda429356bff24b3d235d5587a57f00
Dainius258 <dainiuss258@gmail.com>
2024-05-21 12:45:53 +0300

    Added try catch to auth middleware

--- frontend/middleware/auth.js
+++ frontend/middleware/auth.js
@@ -5,8 +5,11 @@ export default defineNuxtRouteMiddleware((to, from) => {
   const localePath = useLocalePath();
   const authStore = useAuthStore();
 
-  //console.log("Auth middleware is ran, value is:", authStore.isAuthenticated);
-  if (!authStore.isAuthenticated && to.path !== localePath("/login")) {
-    return navigateTo(localePath("/login"));
+  try {
+    if (!authStore.isAuthenticated && to.path !== localePath("/login")) {
+      return navigateTo(localePath("/login"));
+    }
+  } catch (error) {
+    console.log(errpr);
   }
 });
