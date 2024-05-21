0578a2b0d8f656fd24727e25b442f87186abb7e4
Dainius258 <dainiuss258@gmail.com>
2024-05-21 11:51:35 +0300

    Switched to pinia module instead of using plug in

--- frontend/plugins/pinia.js
+++
@@ -1,7 +0,0 @@
-import { defineNuxtPlugin } from "#imports";
-import { createPinia } from "pinia";
-
-export default defineNuxtPlugin((nuxtApp) => {
-  const pinia = createPinia();
-  nuxtApp.vueApp.use(pinia);
-});
