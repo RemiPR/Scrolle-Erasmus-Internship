c9755ef816ab3902a5c92918e244ddf0628eadb5
Dainius258 <dainiuss258@gmail.com>
2024-05-23 11:10:26 +0300

    Added dynamic tab for guest faq page

--- frontend/pages/guest/faq.vue
+++ frontend/pages/guest/faq.vue
@@ -1,1 +1,25 @@
-<template></template>
+<template>
+<div class="px-4 bg-white dark:bg-gray-800 pt-40">
+    <main class="text-center text-black dark:text-white w-4/6 mx-auto pb-32 mx">
+      <h2 class="text-4xl font-bold mb-14">Frequently Asked Questions</h2>
+      <DynamicTab :tabs="faqTypeTabs" />
+    </main>
+  </div>
+</template>
+<script setup>
+import DynamicTab from "@/components/shared/ui/DynamicTab.vue";
+import SharedUiFaq from "@/components/shared/ui/faq.vue";
+
+const faqTypeTabs = [
+  {
+    name: "freecourses",
+    label: "About free online courses",
+    component: SharedUiFaq,
+  },
+  {
+    name: "admission",
+    label: "About admission",
+    //component: EntranceRequirements,
+  },
+];
+</script>
