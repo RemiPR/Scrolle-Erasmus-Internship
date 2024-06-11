e9e6136b2885c9d6a69a72516177bfd5df78630b
Dainius258 <dainiuss258@gmail.com>
2024-06-11 14:29:23 +0300

    backend/auth-service: Improved authentication middleware to perform token refresh on certain conditions

--- backend/auth-service/src/middleware/authentication.js
+++ backend/auth-service/src/middleware/authentication.js
@@ -1,19 +1,71 @@
 import jwt from "jsonwebtoken";
+import { authUtils } from "../utils/authUtils.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // Middleware to verify JWT and extract user info
-export const authenticateToken = (request, response, next) => {
-  const token = request.cookies.authToken; // Retrieve the JWT from a cookie
-  if (!token) {
-    return response
-      .status(401)
-      .json({ message: "Unauthorized: No token provided" });
-  }
+// It also provides a new access token if current one is expired and refresh token is valid
+export const authenticateToken = async (request, response, next) => {
+  const accessToken = request.cookies.authToken; // Retrieve the JWT from a cookie
+  const refreshToken = request.cookies.authRefresh;
+
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET); // Verify the JWT
-    request.user = decoded; // Attach user info to the request
-    next(); // Proceed to the next middleware or route handler
+    if (!accessToken) {
+      if (!refreshToken) {
+        return response
+          .status(401)
+          .send({ message: "Unautorized: No token provided" });
+      }
+      const newAccessToken = await authUtils.refreshAccessToken(refreshToken);
+
+      response.cookie("authToken", newAccessToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "lax",
+        maxAge: process.env.COOKIE_AGE_ACCESS,
+      });
+
+      const decoded = jwt.verify(newAccessToken, process.env.JWT_ACCESS_SECRET);
+      request.user = decoded; // Attach user info to the request
+
+      next(); // Proceed to the next middleware or route handler
+    } else {
+      const decoded = jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET);
+      request.user = decoded;
+
+      next();
+    }
   } catch (error) {
-    console.error("JWT verification error:", error);
-    return response.status(401).json({ message: "Unauthorized" });
+    if (error.name === "TokenExpiredError") {
+      try {
+        if (!refreshToken) {
+          return response
+            .status(401)
+            .send({ message: "Unauthorized: No token provided" });
+        }
+        const newAccessToken = authUtils.refreshAccessToken(refreshToken);
+
+        response.cookie("authToken", newAccessToken, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "lax",
+          maxAge: process.env.COOKIE_AGE_ACCESS,
+        });
+
+        const decoded = jwt.verify(
+          newAccessToken,
+          process.env.JWT_ACCESS_SECRET
+        );
+        request.user = decoded;
+
+        next();
+      } catch (refreshError) {
+        return response.status(401).json({ message: "Unauthorized" });
+      }
+    } else {
+      console.error("JWT verification error:", error);
+      return response.status(401).json({ message: "Unauthorized" });
+    }
   }
 };
