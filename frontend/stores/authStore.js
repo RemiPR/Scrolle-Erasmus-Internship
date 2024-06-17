07607b3026e49c65b4a43803b1a9d186c14e3ecc
Dainius258 <dainiuss258@gmail.com>
2024-06-17 18:14:28 +0300

    stores: authStore properly implemented baseURL for http requests

--- frontend/stores/authStore.js
+++ frontend/stores/authStore.js
@@ -25,7 +25,7 @@ export const useAuthStore = defineStore("auth", {
     },
     async addPersonalInfo(form, authBaseUrl) {
       try {
-        await $fetch(`${authBaseUrl}/api/auth/guest/addPersonalInfo`, {
+        await $fetch(`/api/auth/guest/addPersonalInfo`, {
           method: "POST",
           body: {
             name: form.value.firstName,
@@ -36,6 +36,7 @@ export const useAuthStore = defineStore("auth", {
             education: form.value.education,
             reason: form.value.reason,
           },
+          baseURL: authBaseUrl,
           credentials: "include",
         });
         this.user.isConfirmed = true;
@@ -46,8 +47,9 @@ export const useAuthStore = defineStore("auth", {
     async loginGuest(email, password, redirectPath, authBaseUrl) {
       try {
         // http request to login
-        await $fetch(`${authBaseUrl}/api/auth/guest/loginUser`, {
+        await $fetch(`/api/auth/guest/loginUser`, {
           method: "POST",
+          baseURL: authBaseUrl,
           body: {
             email,
             password,
@@ -77,8 +79,9 @@ export const useAuthStore = defineStore("auth", {
     },
     async logoutGuest(redirectPath, authBaseUrl) {
       try {
-        await $fetch(`${authBaseUrl}/api/auth/guest/logoutUser`, {
+        await $fetch(`/api/auth/guest/logoutUser`, {
           method: "POST",
+          baseURL: authBaseUrl,
         });
         this.clearUser();
         navigateTo(redirectPath);
@@ -89,9 +92,10 @@ export const useAuthStore = defineStore("auth", {
     async refreshToken(authBaseUrl) {
       const { toLogin } = useRedirectPath();
       try {
-        await $fetch(`${authBaseUrl}/api/auth/guest/refresh`, {
+        await $fetch(`/api/auth/guest/refresh`, {
           method: "POST",
           credentials: "include",
+          baseURL: authBaseUrl,
         });
       } catch (error) {
         this.clearUser();
