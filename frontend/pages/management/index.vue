75299c8d7b2725ee2419612ff7b6bbbf80d926af
Remi <prialgauskis.remigijus@gmail.com>
2024-06-25 10:39:16 +0300

    Commented out the management page for deployment test

--- frontend/pages/management/index.vue
+++ frontend/pages/management/index.vue
@@ -1,85 +1,86 @@
 <!-- pages/teacher.vue -->
 <template>
   <div class="relative min-h-screen bg-gray-100">
-    <Sidebar />
+    <!-- <Sidebar /> -->
   </div>
 </template>
 
+//
 <script setup>
-import { ref, markRaw } from "vue";
-import { provideSidebar } from "@/composables/useSidebar";
-import Sidebar from "@/components/global/nav/shared/Sidebar.vue";
-import DashboardComponent from "@/components/DashboardComponent.vue";
-import OnlineClassesComponent from "@/components/OnlineClassesComponent.vue";
-import CoursesComponent from "@/components/CoursesComponent.vue";
-import GroupsComponent from "@/components/GroupsComponent.vue";
-import ErasmusComponent from "@/components/ErasmusComponent.vue";
-import CalendarComponent from "@/components/CalendarComponent.vue";
+// import { ref, markRaw } from "vue";
+// import { provideSidebar } from "@/composables/useSidebar";
+// import Sidebar from "@/components/global/nav/shared/Sidebar.vue";
+// import DashboardComponent from "@/components/DashboardComponent.vue";
+// import OnlineClassesComponent from "@/components/OnlineClassesComponent.vue";
+// import GroupsComponent from "@/components/GroupsComponent.vue";
+// import ErasmusComponent from "@/components/ErasmusComponent.vue";
+// import CalendarComponent from "@/components/CalendarComponent.vue";
 
-import HelpComponent from "@/components/Help.vue";
-import SettingsComponent from "@/components/Settings.vue";
-const sidebarLinks = ref([
-  {
-    label: "Dashboard",
-    component: markRaw(DashboardComponent),
-    icon: "ri:home-3-fill",
-  },
-  {
-    label: "Online Classes",
-    component: markRaw(OnlineClassesComponent),
-    icon: "mdi:video",
-  },
-  {
-    label: "Courses",
-    component: markRaw(CoursesComponent),
-    icon: "mdi:book-open-outline",
-  },
-  {
-    label: "Groups",
-    component: markRaw(GroupsComponent),
-    icon: "mdi:account-group-outline",
-  },
-  {
-    label: "My Erasmus",
-    component: markRaw(ErasmusComponent),
-    icon: "mdi:earth",
-  },
-  {
-    label: "Calendar",
-    component: markRaw(CalendarComponent),
-    icon: "mdi:calendar-outline",
-  },
-  // Add more sidebar links as needed
-]);
+// import HelpComponent from "@/components/Help.vue";
+// import SettingsComponent from "@/components/Settings.vue";
+// const sidebarLinks = ref([
+//   {
+//     label: "Dashboard",
+//     component: markRaw(DashboardComponent),
+//     icon: "ri:home-3-fill",
+//   },
+//   {
+//     label: "Online Classes",
+//     component: markRaw(OnlineClassesComponent),
+//     icon: "mdi:video",
+//   },
+//   {
+//     label: "Courses",
+//     component: markRaw(CoursesComponent),
+//     icon: "mdi:book-open-outline",
+//   },
+//   {
+//     label: "Groups",
+//     component: markRaw(GroupsComponent),
+//     icon: "mdi:account-group-outline",
+//   },
+//   {
+//     label: "My Erasmus",
+//     component: markRaw(ErasmusComponent),
+//     icon: "mdi:earth",
+//   },
+//   {
+//     label: "Calendar",
+//     component: markRaw(CalendarComponent),
+//     icon: "mdi:calendar-outline",
+//   },
+//   // Add more sidebar links as needed
+// ]);
 
-const additionalLinks = ref([
-  {
-    label: "Help & Support",
-    component: markRaw(HelpComponent),
-    icon: "mdi:help-circle-outline",
-  },
-  {
-    label: "Settings",
-    component: markRaw(SettingsComponent),
-    icon: "mdi:cog-outline",
-  },
-]);
+// const additionalLinks = ref([
+//   {
+//     label: "Help & Support",
+//     component: markRaw(HelpComponent),
+//     icon: "mdi:help-circle-outline",
+//   },
+//   {
+//     label: "Settings",
+//     component: markRaw(SettingsComponent),
+//     icon: "mdi:cog-outline",
+//   },
+// ]);
 
-const teacherInfo = {
-  name: "Elīna Bērziņa",
-  email: "elina.berzina@example.com",
-};
+// const teacherInfo = {
+//   name: "Elīna Bērziņa",
+//   email: "elina.berzina@example.com",
+// };
 
-provideSidebar(
-  sidebarLinks.value,
-  additionalLinks.value,
-  sidebarLinks.value[0].component,
-  teacherInfo
-);
+// provideSidebar(
+//   sidebarLinks.value,
+//   additionalLinks.value,
+//   sidebarLinks.value[0].component,
+//   teacherInfo
+// );
 
-definePageMeta({
-  layout: "nav",
-  navigation: "management",
-  enableScrollStyling: true,
-});
+// definePageMeta({
+//   layout: "nav",
+//   navigation: "management",
+//   enableScrollStyling: true,
+// });
+//
 </script>
